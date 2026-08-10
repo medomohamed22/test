813e477887370b15e4f08dbfd4d3e4f1b2558894
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-08-10 09:13:21 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-
-// Vercel imports the Express app. Locally we start a normal HTTP server.
-if (!process.env.VERCEL) {
-  const port = Number(process.env.PORT || 4000);
-  app.listen(port, () => console.log(`DealWay running on http://localhost:${port}`));
-}
-
-export default app;
